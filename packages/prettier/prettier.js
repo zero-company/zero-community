ca09704cfa584cceea054dd848f89b4d17da0969
Jigz <jigzpalillo@gmail.com>
2022-11-17 23:36:13 +0800

    [prettier] Migrate prettier-v1 to package

---
+++ packages/prettier/prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _references: [
+    'https://www.npmjs.com/package/@zero-company/prettier',
+    'https://github.com/zero-company/zero-community',
+  ],
+  $schema: 'http://json.schemastore.org/prettierrc',
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+}
