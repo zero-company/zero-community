a9672413e3e52a572425fe3e4857457165fa9463
Jigz <jigzpalillo@gmail.com>
2022-11-19 21:36:10 +0800

    [zero-colors] Add zero-colors.ts

---
+++ packages/zero-colors/zero-colors.ts
@@ -0,0 +1,32 @@
+/**
+ * References
+ * https://spectrum.adobe.com/page/color-fundamentals
+ * https://tailwindcss.com/docs/customizing-colors
+ *
+ * TODO: Fix missing deprecation flag from 'as const'
+ * https://github.com/microsoft/TypeScript/issues/39374
+ * https://github.com/microsoft/TypeScript/pull/46167
+ */
+
+import { material } from './material'
+import { spectrum } from './spectrum'
+import * as tailwind from 'tailwindcss/colors'
+
+/**
+ * TODO: Fix strict color typechecking without replacing type assertion
+ * as const satisfies Record<string, ColorType<typeof index>>
+ * as const satisfies ColorType<typeof index>
+ * https://github.com/microsoft/TypeScript/issues/31062
+ */
+export type IndexType = readonly string[]
+export type ColorType<I extends IndexType> = Record<I[number], string>
+
+/**
+ * Modified Tailwind + Material color palette
+ */
+const zeroColors = {
+  ...tailwind,
+  material,
+  /** @deprecated As of zero-colors v1.0.1, single flat palettes are recommended. */
+  spectrum,
+}
