ea15d0832b9ec37ff82e4fc5b2a380e4d88b8681
Jigz <jigzpalillo@gmail.com>
2023-03-02 17:50:55 +0800

    [zero-official] Add zero-official.ts

---
+++ packages/zero-official/src/zero-official.ts
@@ -0,0 +1,11 @@
+export const ZeroOfficialLinks = {
+	github: 'https://github.com/zero-company',
+	website: 'https://zero-company.pages.dev',
+	patreon: 'https://www.patreon.com/ZeroCompany',
+	discord: 'https://discord.gg/2a5HcmxvgC',
+	twitter: 'https://twitter.com/ZeroComms',
+	facebook: 'https://facebook.com/ZeroComms',
+	email: "Unavailable, Let's talk on discord",
+	domain: 'Unavailable, Cybersquatters make it hard to find a domain',
+	publicApi: 'Unavailable, Coming Soon',
+}
