19b9d73cef94fbff9755cddb21ea34d94f240ced
Jigz <jigzpalillo@gmail.com>
2023-03-13 14:29:08 +0800

    [zero-types] Add blog-post-type-v1.ts

--- packages/zero-types/src/types/blog-page-type-v1.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const zodBlogPageTypeV1 = z.object({
-	title: z.string(),
-	slug: z.string(),
-	markdown: z.string(),
-})
-
-export type BlogPageTypeV1 = z.infer<typeof zodBlogPageTypeV1>
