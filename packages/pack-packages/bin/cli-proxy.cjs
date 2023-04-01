a9d00ddead6e0d74da9410ac3c7bd0b27d0659df
Jigz <jigzpalillo@gmail.com>
2023-04-02 03:37:55 +0800

    [pack-packages] Rename pack-package

--- packages/pack-packages/bin/cli-proxy.cjs
+++ packages/pack-packages/bin/cli-proxy.cjs
@@ -4,4 +4,4 @@
  * How to create a pipeline when one build produces a "bin" which is used by another build?
  * https://github.com/vercel/turbo/discussions/446
  */
-require('./../dist/pack-package-cli.cjs')
+require('./../dist/pack-packages-cli.cjs')
