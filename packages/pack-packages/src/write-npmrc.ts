37ec4c0b39a2bb2e442c081249d40339c247fa4e
Jigz <jigzpalillo@gmail.com>
2023-04-02 08:14:17 +0800

    [pack-packages] Update write-npmrc.ts

--- packages/pack-packages/src/write-npmrc.ts
+++ packages/pack-packages/src/write-npmrc.ts
@@ -1,12 +1,13 @@
 import * as fse from 'fs-extra'
 import packagejson from './../package.json'
 import * as path from 'node:path'
+import * as url from 'node:url'
 
 type WriteNpmrcOptions = {
 	/** output path for .npmrc i.e. packages/mypackage */
 	outputPath: string
-	/** registry to be used i.e. npm.pkg.github.com, registry.npmjs.org */
-	registry: string
+	/** registry to be used i.e. https://npm.pkg.github.com, https://registry.npmjs.org */
+	registryUrl: string
 	/** name of env variable for authToken i.e. NPM_TOKEN, GITHUB_TOKEN */
 	authTokenEnvName: string
 	/** package scope i.e. \@username, \@org */
@@ -16,23 +17,26 @@ type WriteNpmrcOptions = {
 /**
  * 	Write .npmrc
  *	@param opts.outputPath - output path for .npmrc i.e. packages/mypackage
- *	@param opts.registry - registry to be used i.e. npm.pkg.github.com, registry.npmjs.org
+ *	@param opts.registryUrl - registry to be used i.e. https://npm.pkg.github.com, https://registry.npmjs.org
  * 	@param opts.authTokenEnvName - name of env variable for authToken i.e. NPM_TOKEN, GITHUB_TOKEN
  * 	@param opts.scope - package scope i.e. \@username, \@org
  * 	@returns Promise from fs-extra
  */
 export const writeNpmrc = ({
 	outputPath,
-	registry,
+	registryUrl,
 	scope,
 	authTokenEnvName,
 }: WriteNpmrcOptions) => {
 	// TODO: Publish and use standard log package
 	// TODO: Check if env variable exists, if not warn
+
+	const { host: registryHost } = url.parse(registryUrl)
+
 	const data = `
 	# [${packagejson.name}][start] Set registry, remove after use
-		${scope}:registry=https://${registry}
-		//${registry}/:_authToken=\${${authTokenEnvName}}
+		${scope}:registry=${registryUrl}
+		//${registryHost}/:_authToken=\${${authTokenEnvName}}
 	# [${packagejson.name}][end] Set registry
 `
 	const outputFile = path.join(outputPath, '.npmrc')
