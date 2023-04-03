5335e01fdd4a3d46395ebd05965e813fd8fdf090
Jigz <jigzpalillo@gmail.com>
2023-04-03 20:11:01 +0800

    [pack-packages] Update pack-packages.ts

---
+++ packages/pack-packages/src/pack-package-group.ts
@@ -0,0 +1,43 @@
+import {
+	PackageGroupType,
+	parsePackageGroup,
+	validatePackageGroup,
+} from './package-group'
+import { writeNpmrc } from './write-npmrc'
+import { npmPack } from './npm-pack'
+import * as path from 'node:path'
+import * as fse from 'fs-extra'
+import packagejson from './../package.json'
+
+export const packPackageGroup = ({
+	packageGroup,
+	index,
+	cacheDirectory,
+}: {
+	packageGroup: PackageGroupType
+	index: number
+	cacheDirectory: string
+}) =>
+	new Promise<void>((resolve, reject) => {
+		const parsedPackageGroup = parsePackageGroup(packageGroup, index)
+
+		if (validatePackageGroup(parsedPackageGroup)) {
+			const { packages, registryUrl, authTokenEnvName } = parsedPackageGroup
+			const scopes = packages
+				.filter(el => el.startsWith('@'))
+				.map(el => el.split('/')[0])
+			const cwd = path.join(cacheDirectory, `package-group-${index}`)
+
+			writeNpmrc({
+				outputPath: cwd,
+				registryUrl,
+				scopes,
+				authTokenEnvName,
+			})
+				.then(() => npmPack({ cwd, packages }))
+				.then(() => fse.remove(cwd))
+				.then(() => resolve())
+		} else {
+			reject(`[${packagejson.name}] Failed to pack PackageGroup[${index}]`)
+		}
+	})
--- packages/pack-packages/src/pack-packages.ts
+++ packages/pack-packages/src/pack-packages.ts
@@ -1,54 +1,14 @@
 import packagejson from './../package.json'
-import {
-	PackageGroupType,
-	parsePackageGroup,
-	validatePackageGroup,
-} from './package-group'
-import { writeNpmrc } from './write-npmrc'
-import { npmPack } from './npm-pack'
-import * as path from 'node:path'
+import { PackageGroupType } from './package-group'
 import * as fse from 'fs-extra'
+import { packPackageGroup } from './pack-package-group'
 
 export type PackPackagesOptions = {
 	packageGroups: PackageGroupType[]
 }
 
-const packPackageGroup = ({
-	packageGroup,
-	index,
-	cacheDirectory,
-}: {
-	packageGroup: PackageGroupType
-	index: number
-	cacheDirectory: string
-}) =>
-	new Promise<void>((resolve, reject) => {
-		const parsedPackageGroup = parsePackageGroup(packageGroup, index)
-
-		if (validatePackageGroup(parsedPackageGroup)) {
-			const { packages, registryUrl, authTokenEnvName } = parsedPackageGroup
-			const scopes = packages
-				.filter(el => el.startsWith('@'))
-				.map(el => el.split('/')[0])
-			const cwd = path.join(cacheDirectory, `package-group-${index}`)
-
-			writeNpmrc({
-				outputPath: cwd,
-				registryUrl,
-				scopes,
-				authTokenEnvName,
-			})
-				.then(() => npmPack({ cwd, packages }))
-				.then(() => fse.remove(cwd))
-				.then(() => resolve())
-		} else {
-			reject(`[${packagejson.name}] Failed to pack PackageGroup[${index}]`)
-		}
-	})
-
 /**
  * 	Pack packages in tgz
- *
  */
 export const packPackages = ({ packageGroups }: PackPackagesOptions) => {
 	const cacheDirectory = `node_modules/.cache/zero/pack-packages`
