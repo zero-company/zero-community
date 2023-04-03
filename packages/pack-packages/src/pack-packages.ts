9d93c7f331d4eb51d27e093dcab1816e3c2bc14d
Jigz <jigzpalillo@gmail.com>
2023-04-03 23:25:07 +0800

    [pack-packages] Update pack-packages.ts

--- packages/pack-packages/src/pack-packages.ts
+++ packages/pack-packages/src/pack-packages.ts
@@ -4,12 +4,31 @@ import * as fse from 'fs-extra'
 import { packPackageGroup } from './pack-package-group'
 
 export type PackPackagesOptions = {
+	/**
+	 * @param array - array of PackageGroup or PackageGroupString
+	 * @param PackageGroup.packages - array of packages i.e. \@zero-company/zero-ui, tailwindcss
+	 * @param PackageGroup.registryUrl - url of registry i.e. https://npm.pkg.github.com, https://registry.npmjs.org
+	 * @param PackageGroup.authTokenEnvName - name of env variable for authToken i.e. NPM_TOKEN, GITHUB_TOKEN
+	 * @example PackageGroupString
+	 * '[@zero-company/zero-ui,tailwindcss],https://npm.pkg.github.com,ZERO_READONLY_GITHUB_TOKEN'
+	 * @example PackageGroup
+	 * ```
+	 * {
+	 * 	packages: ['@zero-company/zero-ui@1.0.0', 'tailwindcss'],
+	 * 	registryUrl: 'https://npm.pkg.github.com',
+	 * 	authTokenEnvName: 'ZERO_READONLY_GITHUB_TOKEN',
+	 * }
+	 * ```
+	 */
 	packageGroups: PackageGroupType[]
+	/** keep cached PackageGroup directory */
 	cleanup?: boolean
 }
 
 /**
- * 	Pack packages in tgz
+ 	Pack packages in tgz
+	@param {array} opts.packageGroups - array of PackageGroups
+ 	@param opts.cleanup - keep cached PackageGroup directory
  */
 export const packPackages = ({
 	packageGroups,
