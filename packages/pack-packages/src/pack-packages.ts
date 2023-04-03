102f127c008c0c951a75dbf07173d2192d8cf78b
Jigz <jigzpalillo@gmail.com>
2023-04-03 20:07:10 +0800

    [pack-packages] Update pack-packages.ts

--- packages/pack-packages/src/pack-packages.ts
+++ packages/pack-packages/src/pack-packages.ts
@@ -6,18 +6,23 @@ import {
 } from './package-group'
 import { writeNpmrc } from './write-npmrc'
 import { npmPack } from './npm-pack'
-import { cleanup } from './cleanup'
+import * as path from 'node:path'
+import * as fse from 'fs-extra'
 
 export type PackPackagesOptions = {
 	packageGroups: PackageGroupType[]
 }
 
-/**
- * 	Pack packages in tgz
- *
- */
-export const packPackages = ({ packageGroups }: PackPackagesOptions) => {
-	packageGroups.map((packageGroup, index) => {
+const packPackageGroup = ({
+	packageGroup,
+	index,
+	cacheDirectory,
+}: {
+	packageGroup: PackageGroupType
+	index: number
+	cacheDirectory: string
+}) =>
+	new Promise<void>((resolve, reject) => {
 		const parsedPackageGroup = parsePackageGroup(packageGroup, index)
 
 		if (validatePackageGroup(parsedPackageGroup)) {
@@ -25,32 +30,51 @@ export const packPackages = ({ packageGroups }: PackPackagesOptions) => {
 			const scopes = packages
 				.filter(el => el.startsWith('@'))
 				.map(el => el.split('/')[0])
-
-			const cacheDirectory = `node_modules/.cache/zero/pack-packages/package-group-${index}`
+			const cwd = path.join(cacheDirectory, `package-group-${index}`)
 
 			writeNpmrc({
-				outputPath: cacheDirectory,
+				outputPath: cwd,
 				registryUrl,
 				scopes,
 				authTokenEnvName,
 			})
-				.then(() => npmPack({ cwd: cacheDirectory, packages }))
-				.then(() => cleanup(cacheDirectory))
+				.then(() => npmPack({ cwd, packages }))
+				.then(() => fse.remove(cwd))
+				.then(() => resolve())
 		} else {
-			console.error(
-				`[${
-					packagejson.name
-				}] Failed to parse packageGroup[${index}], has unsupported type of ${typeof packageGroup}`,
-			)
+			reject(`[${packagejson.name}] Failed to pack PackageGroup[${index}]`)
 		}
 	})
+
+/**
+ * 	Pack packages in tgz
+ *
+ */
+export const packPackages = ({ packageGroups }: PackPackagesOptions) => {
+	const cacheDirectory = `node_modules/.cache/zero/pack-packages`
+	return fse
+		.remove(cacheDirectory)
+		.then(() =>
+			Promise.all(
+				packageGroups.map((packageGroup, index) =>
+					packPackageGroup({ packageGroup, index, cacheDirectory }),
+				),
+			),
+		)
+		.then(res => {
+			console.log(`[${packagejson.name}] Finished packing`)
+			return res
+		})
+		.catch(error => {
+			console.log(error)
+		})
 }
 
 packPackages({
 	packageGroups: [
 		'[@zero-company/zero-id],https://registry.npmjs.org,ZERO_READONLY_GITHUB_TOKEN_V1',
 		{
-			packages: ['@zero-company/zero-ui', 'tsup'],
+			packages: ['@zero-company/zero-ui@1.0.0', 'tsup'],
 			registryUrl: 'https://npm.pkg.github.com',
 			authTokenEnvName: 'ZERO_READONLY_GITHUB_TOKEN_V1',
 		},
