47decc109938ee0f720e7a048cc0b8a3b333a507
Jigz <jigzpalillo@gmail.com>
2023-07-08 08:25:04 +0800

    [zero-colors] Update zero-colors.ts

--- packages/zero-colors/src/zero-colors.ts
+++ packages/zero-colors/src/zero-colors.ts
@@ -10,7 +10,7 @@
 
 import { material } from './material'
 import { spectrum } from './spectrum'
-import * as tailwind from 'tailwindcss/colors'
+import * as tailwindColors from 'tailwindcss/colors'
 
 /**
  * TODO: Fix strict color typechecking without replacing type assertion
@@ -21,12 +21,29 @@ import * as tailwind from 'tailwindcss/colors'
 export type IndexType = readonly string[]
 export type ColorType<I extends IndexType> = Record<I[number], string>
 
+const zero = {
+	primary: tailwindColors.zinc[100],
+	bg: tailwindColors.zinc[950],
+	divider: tailwindColors.zinc[900],
+	surface: tailwindColors.zinc[900],
+	surfaceHover: tailwindColors.zinc[800],
+	surfaceFocus: tailwindColors.zinc[700],
+	info: tailwindColors.blue[600],
+	success: tailwindColors.green[600],
+	warning: tailwindColors.yellow[500],
+	error: tailwindColors.red[600],
+	colorDefault: tailwindColors.zinc[400],
+	colorHover: tailwindColors.zinc[100],
+	colorFocus: tailwindColors.zinc[100],
+}
+
 /**
  * Modified Tailwind + Material color palette
  */
 export const zeroColors = {
-	...tailwind,
+	...tailwindColors,
 	material,
 	/** @deprecated As of zero-colors v1.0.1, single flat palettes are recommended. */
 	spectrum,
+	zero
 }
