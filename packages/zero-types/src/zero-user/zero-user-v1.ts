42538dd8a5092b7383ae20285efbc45658ca54e7
Jigz <jigzpalillo@gmail.com>
2023-07-26 12:24:13 +0800

    [zero-types] Add ZeroUserV1

--- packages/zero-types/src/zero-user/zero-user-v1.ts
+++ packages/zero-types/src/zero-user/zero-user-v1.ts
@@ -1,1 +1,14 @@
-export const ZeroUserV1 = {}
+import { z } from 'zod'
+
+export const ZeroUserV1 = z.object({
+	email: z.string(),
+	emailVerified: z.date(),
+	name: z.string(),
+	fullName: z.object({
+		firstName: z.string(),
+		secondName: z.string(),
+		middleName: z.string(),
+		lastName: z.string(),
+	}),
+	sex: z.optional(z.enum(['male', 'female'])),
+})
