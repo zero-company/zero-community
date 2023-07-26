ecdcd2a9e712787bbfb9e52a03fb88416529e70a
Jigz <jigzpalillo@gmail.com>
2023-07-26 11:47:57 +0800

    [zero-types] Update ZeroIndexV1

---
+++ packages/zero-types/src/zeroIndex/zero-index-v1.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+export const createZeroIndexV1Zod = <T extends z.ZodTypeAny>(ZeroType: T) => {
+	return z.object({
+		// Universally unique zero-id, prefix zerox-xxxxxx, generated and reserved on server side
+		zeroId: z.string(),
+		// Valid ZeroTypeVx zero-id
+		zeroTypeId: z.string(),
+		// Optional valid VersionTypeVx zero-id
+		versionId: z.optional(z.string()),
+		// Optional valid TagTypeVx zero-ids or string
+		tagsId: z.optional(z.array(z.string())),
+		// Required UserTypeVx zero-id
+		ownerId: z.string(),
+		// Optional UserTypeVx zero-ids
+		accessUpdateId: z.optional(z.array(z.string())),
+		// Optional UserTypeVx zero-ids
+		accessFetchId: z.optional(z.array(z.string())),
+		// Required name
+		name: z.string(),
+		// Optional description
+		description: z.optional(z.string()),
+		// Optional value object
+		value: z.optional(ZeroType),
+		// Used by cleaning purge bot
+		expirationDate: z.optional(z.date()),
+	})
+}
+
+export const ZeroIndexV1Zod = createZeroIndexV1Zod(z.any())
+
+export type ZeroIndexV1Type = z.infer<typeof ZeroIndexV1Zod>
+
