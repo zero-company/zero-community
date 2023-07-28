a152397526599b847f8262ad36e0102fa8c42f66
Jigz <jigzpalillo@gmail.com>
2023-07-28 18:00:17 +0800

    [zero-types] Add ZeroAccountV1

---
+++ packages/zero-types/src/zero-account/zero-account-v1.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const ZeroAccountV1Zod = z.object({
+	email: z.string(),
+	emailVerified: z.optional(z.date()),
+	userName: z.string(),
+	fullName: z.optional(
+		z.object({
+			firstName: z.optional(z.string()),
+			secondName: z.optional(z.string()),
+			middleName: z.optional(z.string()),
+			lastName: z.optional(z.string()),
+		}),
+	),
+	sex: z.optional(z.enum(['male', 'female'])),
+	password: z.string(),
+})
