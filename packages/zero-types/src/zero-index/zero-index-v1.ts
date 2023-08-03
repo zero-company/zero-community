7e2f149de6a0c6657990fcc0d785d5b1fcaf36dd
Jigz <jigzpalillo@gmail.com>
2023-08-03 19:52:50 +0800

    [zero-types] Update ZeroIndexV1

--- packages/zero-types/src/zero-index/zero-index-v1.ts
+++ packages/zero-types/src/zero-index/zero-index-v1.ts
@@ -3,16 +3,19 @@ import { z } from 'zod'
 export const createZeroIndexV1Zod = <T extends z.ZodTypeAny>(ValuesZod: T) => {
 	return z.object({
 		zeroId: z.string(),
-		zeroType: z.string(),
-		versionId: z.optional(z.string()),
-		tagsId: z.optional(z.array(z.string())),
 		ownerId: z.string(),
 		accessUpdateId: z.optional(z.array(z.string())),
 		accessFetchId: z.optional(z.array(z.string())),
+		creationDate: z.optional(z.date()),
+		lastModifiedDate: z.optional(z.date()),
+		expirationDate: z.optional(z.date()),
 		name: z.string(),
 		description: z.optional(z.string()),
+		versionId: z.optional(z.string()),
+		tagsId: z.optional(z.array(z.string())),
+		zeroType: z.string(),
 		values: ValuesZod,
-		expirationDate: z.optional(z.date()),
+
 	})
 }
 
