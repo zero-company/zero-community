4905b2833c53f5b0b558f1ca1e3eacaf2fa11b61
Jigz <jigzpalillo@gmail.com>
2023-08-04 14:03:11 +0800

    [zero-types] Update ZeroIndexV1 (#59)
    * [zero-types] Update ZeroIndexV1
    
    * [zero-types] Update ZeroIndexV1
    
    * [zero-types] Update ZeroIndexV1
    
    * [changeset] Add khaki-poets-stare.md

--- packages/zero-types/src/zero-index/zero-index-v1.ts
+++ packages/zero-types/src/zero-index/zero-index-v1.ts
@@ -2,28 +2,19 @@ import { z } from 'zod'
 
 export const createZeroIndexV1Zod = <T extends z.ZodTypeAny>(ValuesZod: T) => {
 	return z.object({
-		// Universally unique zero-id, prefix zerox-xxxxxx, generated and reserved on server side
 		zeroId: z.string(),
-		// Valid ZeroTypeVx
-		zeroType: z.string(),
-		// Optional valid VersionTypeVx zero-id
-		versionId: z.optional(z.string()),
-		// Optional valid TagTypeVx zero-ids or string
-		tagsId: z.optional(z.array(z.string())),
-		// Required UserTypeVx zero-id
 		ownerId: z.string(),
-		// Optional UserTypeVx zero-ids
 		accessUpdateId: z.optional(z.array(z.string())),
-		// Optional UserTypeVx zero-ids
 		accessFetchId: z.optional(z.array(z.string())),
-		// Required name
-		name: z.string(),
-		// Optional description
-		description: z.optional(z.string()),
-		// Required value
-		values: ValuesZod,
-		// Used by cleaning purge bot
+		creationDate: z.optional(z.date()),
+		lastModifiedDate: z.optional(z.date()),
 		expirationDate: z.optional(z.date()),
+		name: z.string(),
+		description: z.optional(z.string()),
+		versionId: z.optional(z.string()),
+		tagsId: z.optional(z.array(z.string())),
+		zeroType: z.string(),
+		values: ValuesZod,
 	})
 }
 
