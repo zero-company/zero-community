0274d03a3574f6018ef53c700a840ac757b1ba41
Jigz <jigzpalillo@gmail.com>
2024-06-01 16:31:23 +0800

    Update unpack

--- rust/packages/unpack-crates/src/unpack.rs
+++ rust/packages/unpack-crates/src/unpack.rs
@@ -1,10 +1,18 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use include_dir::{include_dir, Dir};
 use std::path::Path;
-//use tar::Archive;
+
+static CARGO_MANIFEST_DIR: Dir<'static> = include_dir!("$CARGO_MANIFEST_DIR");
 
 pub fn unpack(path: impl AsRef<Path>) -> Result<(), anyhow::Error> {
     let path_ref = path.as_ref();
     fs_extra::dir::create_all(path_ref, true)?;
+
+    CARGO_MANIFEST_DIR
+        .extract(path_ref)
+        .with_context(|| format!("Could not unpack into '{path_ref:?}'"))?;
+
+    println!("test unpack {:?}", file!());
 
     Ok(())
 }
